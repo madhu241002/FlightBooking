dead04e146b59054a60b12d65e4fae0793e35a01
madhu241002 <135446980+madhu241002@users.noreply.github.com>
2023-06-03 22:00:19 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,4 @@
 const mongoose = require("mongoose");
-// const uri = "mongodb+srv://gadeutham2000:gadeutham2000@cluster0.gyt1xbr.mongodb.net/FlightBooking?retryWrites=true&w=majority";
-// mongoose.connect(uri).then(() => {
-//     console.log('connected succeded to database');
-// }).catch((error) => {
-//     console.log("error occured in connecting to db" + error)
-// })
 
  var userSchema = new mongoose.Schema(
     {
@@ -33,13 +27,3 @@ const mongoose = require("mongoose");
 
 const Users = mongoose.model('Users',userSchema);
 module.exports = Users;
-
-// const user1 = new Users({
-//     userId : 1,
-//     name : "Utham",
-//     email : "gade.utham.2000@gmail.com",
-//     password : "random",
-//     role : "user"
-// });
-  
-// user1.save();
